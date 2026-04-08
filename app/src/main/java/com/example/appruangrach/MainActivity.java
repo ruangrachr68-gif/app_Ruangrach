2c10be7db1b175693619fe5c17a48779c44628d8
WS-08 <WS-08@gmail.com>
2026-04-08 16:20:31 +0700

    LAB26815247024

--- app/src/main/java/com/example/appruangrach/MainActivity.java
+++ app/src/main/java/com/example/appruangrach/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appruangrach;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        numMessage = 10;
+        Log.i("numMessage = ", "" + numMessage);
+
+        numMessage++;
+        numMessage = numMessage + 1;
+        Log.i("numMessage = ", "" + numMessage);
+
+        boolean isFriend = true;
+        Log.i("isFriend = ", "" + isFriend);
+
+        String contact = "Ruangrach";
+        String message = "This is my first APP ";
+
+        Toast.makeText(this, "Message from " + contact, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Message is " + message, Toast.LENGTH_SHORT).show();
+        Log.i("message:", "In countUp method");
+        int x = 0;
+// Now an apparently infinite while loop
+        while (true) {
+            x++; // Add 1 to x each time
+            Log.i("x = ", "" + x); // print x
+            if (x == 3) {
+                break; // Get me out of here
+            }
+            String mess = joinThese("Method ", "are ", "cool");
+            Log.i("message=", mess);
+
+            float area = getAreaCircle(5f);
+            Log.i("area=", mess + area);
+        }
+    }
+
+    String joinThese(String a, String b, String c) {
+        return a + b + c;
+    }
+
+    float getAreaCircle(float radius) {
+        return 3.14f * radius * radius;
     }
 }
+
+
